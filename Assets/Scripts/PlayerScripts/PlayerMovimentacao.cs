8069e1d0ff14a0a5ebba4d18152711f225d67245
Pedro Henrique <pedro.andrade@nave.org.br>
2022-04-02 13:15:58 -0300

    Movimentação 1.0

--- Assets/Scripts/PlayerScripts/PlayerMovimentacao.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovimentacao.cs
@@ -5,31 +5,102 @@ using UnityEngine;
 public class PlayerMovimentacao : MonoBehaviour
 {
     [SerializeField]
-    private float velocidade;
-    [SerializeField]
-    private float forcaPulo;
-    private CapsuleCollider playerCollider;
+    private float velocidade = 5f;
+    private Rigidbody playerRigidbody;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        playerCollider = GetComponent<CapsuleCollider>();
-        ChecarChao();
+        Moviment();
     }
-    private void ChecarChao()
+    public void Moviment()
     {
-        Ray ray = new Ray(new Vector3(playerCollider.transform.position.x, playerCollider.transform.position.y), new Vector3(0,-1,0));
-        RaycastHit hit;
-        if(playerCollider.Raycast(ray, out hit, 20f))
+        if (Input.GetAxis("Vertical") > 0f)
         {
-            Debug.Log("aaaaaa");
-
+            //// para cima
+            if (Input.GetAxis("Horizontal") > 0f)
+            {
+                // para cima direita
+                playerRigidbody.velocity = new Vector3(velocidade, 0f, velocidade);
+            }
+            else if (Input.GetAxis("Horizontal") < 0f)
+            {
+                // para cima esquerda
+                playerRigidbody.velocity = new Vector3(-velocidade, 0f, velocidade);
+            }
+            else
+            {
+                playerRigidbody.velocity = new Vector3(0f, 0f, velocidade);
+            }
+        }
+        else if (Input.GetAxis("Vertical") < 0f)
+        {
+            ////para baixo
+            if (Input.GetAxis("Horizontal") > 0f)
+            {
+                // para baixo direita
+                playerRigidbody.velocity = new Vector3(velocidade, 0f, -velocidade);
+            }
+            else if (Input.GetAxis("Horizontal") < 0f)
+            {
+                // para baixo esquerda
+                playerRigidbody.velocity = new Vector3(-velocidade, 0f, -velocidade);
+            }
+            else
+            {
+                playerRigidbody.velocity = new Vector3(0f, 0f, -velocidade);
+            }
+        }
+        else
+        {
+            playerRigidbody.velocity = new Vector3(0f, 0f, 0f);
+        }
+        if (Input.GetAxis("Horizontal") > 0f)
+        {
+            //// para direita
+            if (Input.GetAxis("Vertical") > 0f)
+            {
+                //para direita cima
+                playerRigidbody.velocity = new Vector3(velocidade, 0f, velocidade);
+            }
+            else if (Input.GetAxis("Vertical") < 0f)
+            {
+                // para direita baixo
+                playerRigidbody.velocity = new Vector3(velocidade, 0f, -velocidade);
+            }
+            else
+            {
+                playerRigidbody.velocity = new Vector3(velocidade, 0f, 0f);
+            }
+        }
+        else if (Input.GetAxis("Horizontal") < 0f)
+        {
+            //// para esquerda
+            if (Input.GetAxis("Vertical") > 0f)
+            {
+                //para esquerda cima
+                playerRigidbody.velocity = new Vector3(-velocidade, 0f, velocidade);
+            }
+            else if (Input.GetAxis("Vertical") < 0f)
+            {
+                // para esquerda baixo
+                playerRigidbody.velocity = new Vector3(-velocidade, 0f, -velocidade);
+            }
+            else
+            {
+                playerRigidbody.velocity = new Vector3(-velocidade, 0f, 0f);
+            }
+        }
+        else
+        {
+            playerRigidbody.velocity = new Vector3(0f, 0f, 0f);
         }
     }
+
 }
